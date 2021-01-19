500ec35ba23fb5d94a3d27756531a27971a01328
bbafford <brock.bafford@gmail.com>
2021-01-18 19:58:47 -0500

    Added agency qualifier

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main(argv):
             'Missing required argument of source file. Command format is: main.py -i <csv file> -d <Busware log directory>')
         sys.exit(2)
     try:
-        opts, args = getopt.getopt(argv, "c:d:hi:", ["ifile=", "dir=", "config="])
+        opts, args = getopt.getopt(argv, "c:dhi:", ["ifile=", "dir=", "config="])
     except getopt.GetoptError:
         print('There was an error.  main.py -i <csv file>')
         print(getopt.GetoptError.msg)
@@ -33,47 +33,46 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
-        elif opt in ("-d", "--dir"):
-            buswarelogdir = arg
+        elif opt in ("-d"):
+            cleardb = True
         elif opt in ("-c"):
             configpath = arg
-
-
 
     config = LoadConfiguration(configpath)
     cnn = pyodbc.connect('Server=' + config['DatabaseServer'] + ';DRIVER={SQL Server};database=' + config['DatabaseName'] + ';UID=' + config['DatabaseLogin'] + ';PWD=' + config['DatabasePW']+';Trusted_Connection=no')
     cr = cnn.cursor()
-
+    if cleardb ==True:
+        print ("Deleting all records from tblDiscards")
+        cr.execute("truncate table tblDiscards")
+        cr.commit()
     #directory = os.fsencode(config["FileDirectory"])
     for logfile in os.listdir(config["FileDirectory"]):
+        #get the agency ID
+        a = logfile.split('-')
+        agency = a[0]
+        print ("loading files for: " + agency)
+        LoadRollups(config["FileDirectory"]+'\\'+logfile, cr, agency)
+        LoadData(config["FileDirectory"]+'\\'+logfile, cr, agency)
 
-        LoadRollups(config["FileDirectory"]+'\\'+logfile, cr, agency="NJT")
-
-        with open(config["FileDirectory"]+'\\'+logfile, newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',')
-
-            LoadDiscardReasons(spamreader, cnn, agency="NJT")
-            LoadData(spamreader, cnn, agency="NJT")
-
+        #with open(config["FileDirectory"]+'\\'+logfile, newline='') as csvfile:
+        #    spamreader = csv.reader(csvfile, delimiter=',')
 
 def LoadRollups(spamreader, cr, agency):
     print("Loading Roll ups")
     found = False
     #create an array of the roll ups, compare it to what is in the database and insert the new ones
-   
+
     f  =pd.read_csv(spamreader)
     keep_col = ['ROLLUP']
     u = f[keep_col]
-    print (u)
     t = u["ROLLUP"].unique()
-    print (t)
 
     #get the list of roll ups and only add those that don't exist
     sql = """ select rollup_name from tblRollups"""
     rows = cr.execute(sql).fetchall()
 
     for dfRow in t:
-        print(dfRow)
+       # print(dfRow)
         for row in rows:
             if dfRow==row:
                 found = True
@@ -89,35 +88,33 @@ def LoadRollups(spamreader, cr, agency):
 def LoadDiscardReasons(spamreader, cnn, agency):
     print("Loading Discard Reasons")
 
-    found = False
-    #create an array of the roll ups, compare it to what is in the database and insert the new ones
-
-    f  =pd.read_csv(spamreader)
-    keep_col = ['ROLLUP']
-    u = f[keep_col]
-    print (u)
-    t = u["ROLLUP"].unique()
-    print (t)
-
-    #get the list of roll ups and only add those that don't exist
-    sql = """ select rollup_name from tblRollups"""
-    rows = cr.execute(sql).fetchall()
-
-    for dfRow in t:
-        print(dfRow)
-        for row in rows:
-            if dfRow==row:
-                found = True
-        if found == False:
-        #add the roll up to the table
-            sql_update = "insert into watcher.dbo.tblRollups(rollup_name) values("+dfRow.astype(str)+ ')'
-            cr.execute(sql_update)
-            cr.commit()
-        if found ==True:
-            found = False
-
-def LoadData(spamreader, cnn, agency):
+def LoadData(spamreader, cr, agency):
     print("Loading Data")
+    rollup_name = ''
+    rollup_id = ''
+    #f = pd.read_csv(spamreader)
+    #keep_col = ['ROLLUP', 'VEHICLE', 'TRIP_KEY', 'XDISCARD_REASON', 'XROUTE', 'XSCHEDULE',
+    #            'XSURVEY_DATE', 'XBUSTOOLS_VER', 'XBUSWARES_VER']
+    #u = f[keep_col]
+
+    #grab the data in the roll table so we can match the ids with the name
+
+    sql = """insert into tblDiscards(rollup_id,vehicle_id,trip_key,discard_reason,route_id,schedule_name, survey_date,bustools_version,busware_version,agency) values ( """
+
+    with open(spamreader) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                line_count += 1
+                sql2 = row[0]+","+row[1]+","+row[4]+",'"+row[8]+"','"+row[10]+"','"+row[17]+"','"+row[18]+"','"+row[44]+"','"+row[45]+"','"+agency+"')"
+                sql3 = sql+sql2
+                cr.execute(sql3)
+                cr.commit()
+        print(f'Processed {line_count} lines.')
 
 def LoadConfiguration(configpath):
     config = ''
